bcc9657dc2d09ec637b5e575ccd862b408731f5d
nikky garg <50827585+neha0123@users.noreply.github.com>
2020-10-05 00:17:16 +0530

    Update q-1.cpp

---
+++ q-1.cpp
@@ -0,0 +1,41 @@
+#WAP to find area of a circle, a rectangle and a triangle, using concept of function overloading.
+#include<bits/stdc++.h>
+using namespace std;
+int area(int);
+int area(int,int);
+float area(float);
+float area(float,float);
+int main()
+{
+        int l,b;
+        float r,base,height;
+        cin>>r;
+        cin>>l>>b;
+        cin>>base>>height;
+        cout<<"\nArea of circle is "<<area(r);
+        cout<<"\nArea of rectangle is "<<area(l,b);
+    cout<<"\nArea of triangle is "<<area(base,height);
+}
+float area(float r)
+{
+    return(3.14*r*r);
+}
+int area(int l,int b)
+{
+    return(l*b);
+}
+float area(float base,float height)
+{
+   return((base*height)/2);
+}
+
+If i took the input as
+radius of circle:2
+length of rectangle:4 breath of rectangle:5
+ base of triangle:2 height of triangle:3
+ 
+ 
+ #output shown as
+Area of circle is 12.56
+Area of rectangle is 20
+Area of triangle is 3
