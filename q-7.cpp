2cb46c57f40b57b87856743f85760b4e558fa757
Sahil <55389335+SahilKr24@users.noreply.github.com>
2020-10-05 23:36:44 +0530

    Update q-7.cpp

--- q-7.cpp
+++ q-7.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 
 using namespace std;
@@ -79,34 +78,3 @@ int main()
     cout<<x<<"  "<<y<<endl;
 
        return 0;
-
-=======
-#include <iostream>
-using namespace std;
-
-void swap(int &n1, int &n2)
-{
-    int temp;
-    temp = n1;
-    n1 = n2;
-    n2 = temp;
-}
-
-int main()
-{
-
-    int a = 1, b = 2;
-
-    cout << "\nBefore swapping" << endl;
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-
-    swap(a, b);
-
-    cout << "\nAfter swapping" << endl;
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-
-    return 0;
->>>>>>> 7795091f388889e08bce490b31e02604f6d78c06
-}
