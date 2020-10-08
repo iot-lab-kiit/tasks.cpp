f637cceddc52be2015b365a0bbf7beb2bfe5e746
CHANDAN KUMAR SINGH <53275158+chandan5569@users.noreply.github.com>
2020-10-08 15:55:24 +0530

    Update q-2.cpp

--- q-2.cpp
+++ q-2.cpp
@@ -1,6 +1,4 @@
 
-//WAP to find volume of a sphere, a cylinder and a cuboid, using function overloading.
-
 
 #include<iostream>
 
@@ -77,76 +75,17 @@ int main()
                         {
                                 cout<<"\n enter the length, width and height for the cuboid : ";
                                 cin>>l>>w>>h;
-                                cout<<"\n volume of cuboid is : "<<volume(l,w,h);
+                                cout<<"\n volume of cuboid : "<<volume(l,w,h);
                                 break;
                         }
 
                         case 4:
                                 exit(0);
                         default:
-                                cout<<"\n please enter a valid choice ";
+                                cout<<"\n enter a valid choice ";
                 }
                 
         }while(ch!=4);
 
         return 0;
 }
-=======
-#include <iostream>
-
-using namespace std;
-int volume(int rad)
-{
-    return((4/3)*3.14*rad*rad);
-}
-int volume(int a, int b, int c)
-{
-    return(a*b*c);
-}
-int volume(int x, int y)
-{
-    return(3.14*x*x*y);
-}
-
-
-int main()
-{
-    int ch;
-    cout<<"1. Sphere\n"<<"2. Cylinder\n"<<"3. Cuboid\n"<<"Enter your choice: ";
-    cin>>ch;
-    switch(ch)
-    {
-        case 1:
-        {
-            int n;
-            cout<<"\nEnter radius: ";
-            cin>>n;
-            cout<<"\nVolume of sphere: "<<volume(n);
-            break;
-        }
-        case 2:
-        {
-            int r,h;
-            cout<<"\nEnter radius and height: ";
-            cin>>r>>h;
-            cout<<"Volume of Cylinder: "<<volume(r,h);
-            break;
-        }
-        case 3:
-        {
-            int l,b,h;
-            cout<<"Enter length,breadth,height:";
-            cin>>l>>b>>h;
-            cout<<volume(l,b,h);
-            break;
-        }
-            
-        
-    }
-    
-    
-    
-
-    return 0;
-}
-
